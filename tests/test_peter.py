d7b6f879384eff40fd2489f2bc34221537ee1d52
Peter Veres <pveres@SI388796.local>
2022-03-25 11:24:59 -0400

    team.py

--- tests/test_peter.py
+++ tests/test_peter.py
@@ -3,7 +3,7 @@ import pytest
 import math
 
 
-@pytest.mark.parametrize("variable_a", "variable_b",range(0, 9),range(0,9))
-def test_peter(a,b):
-    assert team.mpc_peter(a,b).startswith("random function output is: ")
-    assert float(team.mpc_peter(a,b).split()[-1]) == a**3-b**2
+@pytest.mark.parametrize("a",range(0, 9))
+def test_peter(a):
+    assert team.mpc_peter(a)
+    assert float(team.mpc_peter(a)) == a**3-10
